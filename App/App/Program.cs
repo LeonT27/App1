aed2a94fc6057bb2f2809d4c8fd865aa9248ecdb
Luis Miguel Tolentino <luistolentino27@gmail.com>
2017-04-12 21:11:39 -0400

    Creacion de la clase Suma

---
+++ App/App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Suma suma = new Suma();
+            Console.WriteLine(suma.Operacion(1,2));
+        }
+    }
+
+    class Suma
+    {
+        public int Operacion(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
